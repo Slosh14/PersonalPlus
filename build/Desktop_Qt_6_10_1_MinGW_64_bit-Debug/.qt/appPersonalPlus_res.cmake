172e8ab9701abd9a6bd71b9b91c4bfb4ef0b6acb
James <leggrjames@outlook.com>
2025-12-15 08:11:10 +1100

    Initial commit

---
+++ build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appPersonalPlus_res.cmake
@@ -0,0 +1,7 @@
+
+set(target "appPersonalPlus")
+set(working_dir "C:/Users/leggrjames/Documents/PersonalPlus")
+set(src_and_dest_list
+
+)
+set(timestamp_file "C:/Users/leggrjames/Documents/PersonalPlus/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appPersonalPlus_res.txt")
