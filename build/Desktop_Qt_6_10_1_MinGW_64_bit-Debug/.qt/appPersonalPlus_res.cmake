209e6f348baf7f04a317195f4c3b4aff692f3ba7
James <leggrjames@outlook.com>
2025-12-15 08:54:27 +1100

    add resource folder and .qcr

--- build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appPersonalPlus_res.cmake
+++ build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appPersonalPlus_res.cmake
@@ -2,6 +2,8 @@
 set(target "appPersonalPlus")
 set(working_dir "C:/Users/leggrjames/Documents/PersonalPlus")
 set(src_and_dest_list
+    "C:/Users/leggrjames/Documents/PersonalPlus/resources/fonts.qrc"
+    "C:/Users/leggrjames/Documents/PersonalPlus/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/PersonalPlus/resources/fonts.qrc"
 
 )
 set(timestamp_file "C:/Users/leggrjames/Documents/PersonalPlus/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appPersonalPlus_res.txt")
