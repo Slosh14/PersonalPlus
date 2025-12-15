f5d5fb04d63f6ff13fa33caed830e11420730d00
James <leggrjames@outlook.com>
2025-12-15 11:37:19 +1100

    added resources.qcr , fonts folder , all fonts , referenced in qcr , loaded in main.cpp

--- build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/PersonalPlus/Main.qml
+++ build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/PersonalPlus/Main.qml
@@ -5,8 +5,14 @@ ApplicationWindow {
     width: 640
     height: 480
     visible: true
-    title: qsTr("Hello World")
+    title: qsTr("Personal Plus")
 
-
+    Text {
+        text: "Sign In"
+        font.family: "Nexa-Trial"
+        font.pointSize: 24
+        font.weight: Font.Black       // For Bold variant
+        anchors.centerIn: parent
+    }
 
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QFontDatabase>
+#include <QObject>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    // Load all Nexa-Trial fonts from the .qrc resource
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-black.otf");
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-blackitalic.otf");
+
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-bold.otf");
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-bolditalic.otf");
+
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-book.otf");
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-bookitalic.otf");
+
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-extrabold.otf");
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-extrabolditalic.otf");
+
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-extralight.otf");
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-extralightitalic.otf");
+
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-heavy.otf");
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-heavyitalic.otf");
+
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-light.otf");
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-lightitalic.otf");
+
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-regular.otf");
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-regularitalic.otf");
+
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-thin.otf");
+    QFontDatabase::addApplicationFont(":/fonts/nexa-trial-thinitalic.otf");
 
     QQmlApplicationEngine engine;
     QObject::connect(
@@ -12,6 +42,7 @@ int main(int argc, char *argv[])
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
+
     engine.loadFromModule("PersonalPlus", "Main");
 
     return app.exec();
