e7fece11de0fc2d20e03ccc41ef73e1f5a2c1130
James <leggrjames@outlook.com>
2025-12-15 13:49:21 +1100

    added images.qrc and ref an image. Also used that image in login.qml

--- build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appPersonalPlus_res.cmake
+++ build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appPersonalPlus_res.cmake
@@ -4,6 +4,8 @@ set(working_dir "C:/Users/leggrjames/Documents/PersonalPlus")
 set(src_and_dest_list
     "C:/Users/leggrjames/Documents/PersonalPlus/resources/fonts.qrc"
     "C:/Users/leggrjames/Documents/PersonalPlus/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/PersonalPlus/resources/fonts.qrc"
+    "C:/Users/leggrjames/Documents/PersonalPlus/resources/images.qrc"
+    "C:/Users/leggrjames/Documents/PersonalPlus/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/PersonalPlus/resources/images.qrc"
 
 )
 set(timestamp_file "C:/Users/leggrjames/Documents/PersonalPlus/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appPersonalPlus_res.txt")
--- login.qml
+++ login.qml
@@ -6,11 +6,20 @@ Item {
     width: 1920
     height: 1080
 
+    // Base white background
     Rectangle {
         anchors.fill: parent
         color: "white"
     }
 
+    Image {
+        id: loginBackground
+        source: "qrc:/images/loginBackground.jpg"
+        x: 540
+        y: 0
+        width: 1380
+        height: 1080
+    }
     // Test Text using Nexa-Trial font
     Text {
         text: "Sign In"
@@ -20,4 +29,5 @@ Item {
         anchors.centerIn: parent
         color: "black"
     }
+
 }
